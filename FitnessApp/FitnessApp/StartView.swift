a2801167c2d8375c5b8bb62c94ce994516e474a4
Kaloyan <kaloyan.va@abv.bg>
2022-01-19 00:57:33 +0200

    Added starting view to the project

---
+++ FitnessApp/FitnessApp/StartView.swift
@@ -0,0 +1,71 @@
+//
+//  StartView.swift
+//  FitnessApp
+//
+//  Created by Kaloyan Vachkov on 19.01.22.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    @State private var isActivated = false
+    
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("startImage")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Welcome")
+                        .font(.system(size: 60, weight: .bold, design: .monospaced))
+                        .foregroundColor(.white)
+                    Spacer()
+                    NavigationLink(destination: RegisterAndLoginView()) {
+                        HStack {
+                            Image(systemName: "person")
+                                .font(.title)
+                            Text("Login")
+                                .fontWeight(.semibold)
+                                .font(.title)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(LinearGradient(gradient: Gradient(colors: [Color.red,Color("grassyGreen")]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(40)
+                        .shadow(radius: 5.0)
+                    }
+                    NavigationLink(destination: RegisterAndLoginView()) {
+                        HStack {
+                            Image(systemName: "rectangle.and.pencil.and.ellipsis")
+                                .font(.title)
+                            Text("Register")
+                                .fontWeight(.semibold)
+                                .font(.title)
+                        }
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .padding()
+                        .foregroundColor(.white)
+                        .buttonStyle(PlainButtonStyle())
+                        .cornerRadius(40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 40)
+                                .stroke(Color.white, lineWidth: 2)
+                        )
+                    }
+                }
+                .frame(width: 300, height: 600)
+                .navigationBarHidden(true)
+                .navigationTitle("")
+            }
+        }
+    }
+}
+
+struct StartView_Previews: PreviewProvider {
+    static var previews: some View {
+            StartView()
+    }
+}
