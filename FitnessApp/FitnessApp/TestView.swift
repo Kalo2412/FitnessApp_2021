f51d95ad8da97ee251cac00343b52759fdefc904
Kaloyan <kaloyan.va@abv.bg>
2022-01-18 15:10:25 +0200

    comitDemo

---
+++ FitnessApp/FitnessApp/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  FitnessApp
+//
+//  Created by Kaloyan Vachkov on 18.01.22.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("ndjskd")
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
