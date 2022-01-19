54baaa8fe9469deec59ce6a6f4a21dada6198914
Stefania Tsvetkova <nia_flo@abv.bg>
2022-01-19 04:13:17 -0800

    Added HomePageView

---
+++ FitnessApp/FitnessApp/Views/HomePageView.swift
@@ -0,0 +1,24 @@
+//
+//  HomePageView.swift
+//  FitnessApp
+//
+//  Created by Stefania Tsvetkova on 1/19/22.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        NavigationView {
+            Text("Home page")
+                .bold()
+                .font(.title)
+        }
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
