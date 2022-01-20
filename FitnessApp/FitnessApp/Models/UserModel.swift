993e4d3acb9dd483aacf08f586a5ef6c89b0e359
Stefania Tsvetkova <nia_flo@abv.bg>
2022-01-20 12:52:22 -0800

    Added user model

---
+++ FitnessApp/FitnessApp/Models/UserModel.swift
@@ -0,0 +1,33 @@
+//
+//  UserModel.swift
+//  FitnessApp
+//
+//  Created by Stefania Tsvetkova on 1/20/22.
+//
+
+class UserModel {
+    var uid: String = ""
+    var email: String = ""
+    var name: String = ""
+    var age: String = ""
+    var profession: String = ""
+    
+    init(uid: String) {
+        if uid != "" {
+            FirebaseManager.instance.firestore.collection("users").document(uid).getDocument { document, error in
+                guard error == nil else {
+                    return
+                }
+                
+                let data = document?.data()
+                if let data = data {
+                    self.uid = uid
+                    self.email = data["email"] as? String ?? ""
+                    self.name = data["name"] as? String ?? ""
+                    self.age = data["age"] as? String ?? ""
+                    self.profession = data["profession"] as? String ?? ""
+                }
+            }
+        }
+    }
+}
