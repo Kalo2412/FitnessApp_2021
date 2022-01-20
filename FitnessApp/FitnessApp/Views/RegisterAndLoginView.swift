14641bf50065bd32521c2a81124ed8fc980789da
Stefania Tsvetkova <nia_flo@abv.bg>
2022-01-20 04:41:55 -0800

    Added pop up window messages for errors in login and register

--- FitnessApp/FitnessApp/Views/RegisterAndLoginView.swift
+++ FitnessApp/FitnessApp/Views/RegisterAndLoginView.swift
@@ -13,8 +13,8 @@ struct RegisterAndLoginView: View {
     @State private var registerModel = RegisterModel()
     @State private var loginModel = LoginModel()
     
-    @State private var loginErrorMessage = ""
-    @State private var registerErrorMessage = ""
+    @State private var errorMessage = ""
+    @State private var showPopUpWindow = false
     
     @State private var isActiveForLogin = false
     @State private var isActiveForRegister = false
@@ -24,170 +24,171 @@ struct RegisterAndLoginView: View {
     }
 
     var body: some View {
-        NavigationView {
-            ZStack {
-                Image("startImage")
-                        .resizable()
-                        .aspectRatio(contentMode: .fill)
-                        .edgesIgnoringSafeArea(.all)
-                        .blur(radius: 5)
-                
-                ScrollView {
-                    VStack(spacing: 15) {
-                        Text("Fitness App")
-                            .font(.system(size: 40, weight: .bold, design: .monospaced))
-                            .foregroundColor(Color.white)
-                        
-                        Image("app-icon-white")
-                            .renderingMode(.original)
+        ZStack {
+            NavigationView {
+                ZStack {
+                    Image("startImage")
                             .resizable()
-                            .scaledToFit()
-                            .frame(width: 80, height: 80, alignment: .center)
+                            .aspectRatio(contentMode: .fill)
+                            .edgesIgnoringSafeArea(.all)
+                            .blur(radius: 5)
+                    
+                    ScrollView {
+                        VStack(spacing: 15) {
+                            Text("Fitness App")
+                                .font(.system(size: 40, weight: .bold, design: .monospaced))
+                                .foregroundColor(Color.white)
                             
-                        Spacer()
-                        
-                        if isLoginMode {
-                            Group {
-                                TextField("", text: $loginModel.email)
-                                    .keyboardType(.emailAddress)
-                                    .autocapitalization(.none)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: loginModel.email.isEmpty, placeholder: "Email"))
+                            Image("app-icon-white")
+                                .renderingMode(.original)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80, alignment: .center)
                                 
-                                SecureField("", text: $loginModel.password)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: loginModel.password.isEmpty, placeholder: "Password"))
-                            }
-                            .frame(width: 250)
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 10)
-                            .overlay(
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(Color.white, lineWidth: 2)
-                            )
-                            
                             Spacer()
                             
-                            NavigationLink(destination: HomePageView(), isActive: $isActiveForLogin) {
-                                Button (action: {
-                                    login() { isLoggedIn in
-                                        self.isActiveForLogin = isLoggedIn
-                                    }
-                                }) {
-                                    HStack {
-                                        Spacer()
-                                        Text("Log in")
-                                            .foregroundColor(Color.black)
-                                            .bold()
-                                            .padding(.horizontal, 20)
-                                            .padding(.vertical, 10)
-                                            .background(Color.white)
-                                            .cornerRadius(14)
-                                        Spacer()
-                                    }
+                            if isLoginMode {
+                                Group {
+                                    TextField("", text: $loginModel.email)
+                                        .keyboardType(.emailAddress)
+                                        .autocapitalization(.none)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: loginModel.email.isEmpty, placeholder: "Email"))
+                                    
+                                    SecureField("", text: $loginModel.password)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: loginModel.password.isEmpty, placeholder: "Password"))
                                 }
-                            }
-                            
-                            VStack {
-                                Text("Don't have an account?")
-                                    .foregroundColor(Color.white)
+                                .frame(width: 250)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 10)
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.white, lineWidth: 2)
+                                )
                                 
-                                Button {
-                                    changeModeToRegister()
-                                } label: {
-                                        Text("Register")
-                                            .foregroundColor(Color.white)
-                                            .bold()
+                                Spacer()
+                                
+                                NavigationLink(destination: HomePageView(), isActive: $isActiveForLogin) {
+                                    Button (action: {
+                                        login() { isLoggedIn in
+                                            self.isActiveForLogin = isLoggedIn
+                                        }
+                                    }) {
+                                        HStack {
+                                            Spacer()
+                                            Text("Log in")
+                                                .foregroundColor(Color.black)
+                                                .bold()
+                                                .padding(.horizontal, 20)
+                                                .padding(.vertical, 10)
+                                                .background(Color.white)
+                                                .cornerRadius(14)
+                                            Spacer()
+                                        }
+                                    }
+                                }
+                                
+                                VStack {
+                                    Text("Don't have an account?")
+                                        .foregroundColor(Color.white)
+                                    
+                                    Button {
+                                        changeModeToRegister()
+                                    } label: {
+                                            Text("Register")
+                                                .foregroundColor(Color.white)
+                                                .bold()
+                                    }
                                 }
                             }
-                            
-                            Text(loginErrorMessage)
-                                .foregroundColor(Color.red)
-                                .multilineTextAlignment(.center)
+                            else
+                            {
+                                Group {
+                                    TextField("", text: $registerModel.email)
+                                        .keyboardType(.emailAddress)
+                                        .autocapitalization(.none)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: registerModel.email.isEmpty, placeholder: "Email"))
+                                    
+                                    SecureField("", text: $registerModel.password)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: registerModel.password.isEmpty, placeholder: "Password"))
+                                    
+                                    TextField("", text: $registerModel.name)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: registerModel.name.isEmpty, placeholder: "Name"))
+                                    
+                                    TextField("", text: $registerModel.age)
+                                        .keyboardType(.numberPad)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: registerModel.age.isEmpty, placeholder: "Age"))
+                                    
+                                    TextField("", text: $registerModel.profession)
+                                        .modifier(PlaceholderStyle(showPlaceHolder: registerModel.profession.isEmpty, placeholder: "Proffecion"))
+                                    
+                                }
+                                .frame(width: 250)
+                                .padding(.horizontal, 10)
+                                .padding(.vertical, 10)
+                                .overlay(
+                                RoundedRectangle(cornerRadius: 14)
+                                    .stroke(Color.white, lineWidth: 2)
+                                )
+                                
+                                Spacer()
+                                
+                                NavigationLink(destination: HomePageView(), isActive: $isActiveForRegister) {
+                                    Button (action: {
+                                        register() { isRegistered in
+                                            self.isActiveForRegister = isRegistered
+                                        }
+                                    }) {
+                                        HStack {
+                                            Spacer()
+                                            Text("Register")
+                                                .foregroundColor(Color.black)
+                                                .bold()
+                                                .padding(.horizontal, 20)
+                                                .padding(.vertical, 10)
+                                                .background(Color.white)
+                                                .cornerRadius(14)
+                                            Spacer()
+                                        }
+                                    }
+                                }
+                                
+                                VStack {
+                                    Text("Already have an account?")
+                                        .foregroundColor(Color.white)
+                                    
+                                    Button {
+                                        changeModeToLogin()
+                                    } label: {
+                                            Text("Log in")
+                                                .foregroundColor(Color.white)
+                                                .bold()
+                                    }
+                                }
+                            }
                         }
-                        else
-                        {
-                            Group {
-                                TextField("", text: $registerModel.email)
-                                    .keyboardType(.emailAddress)
-                                    .autocapitalization(.none)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: registerModel.email.isEmpty, placeholder: "Email"))
-                                
-                                SecureField("", text: $registerModel.password)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: registerModel.password.isEmpty, placeholder: "Password"))
-                                
-                                TextField("", text: $registerModel.name)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: registerModel.name.isEmpty, placeholder: "Name"))
-                                
-                                TextField("", text: $registerModel.age)
-                                    .keyboardType(.numberPad)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: registerModel.age.isEmpty, placeholder: "Age"))
-                                
-                                TextField("", text: $registerModel.profession)
-                                    .modifier(PlaceholderStyle(showPlaceHolder: registerModel.profession.isEmpty, placeholder: "Proffecion"))
-                                
-                            }
-                            .frame(width: 250)
-                            .padding(.horizontal, 10)
-                            .padding(.vertical, 10)
-                            .overlay(
-                            RoundedRectangle(cornerRadius: 14)
-                                .stroke(Color.white, lineWidth: 2)
-                            )
-                            
-                            Spacer()
-                            
-                            NavigationLink(destination: HomePageView(), isActive: $isActiveForRegister) {
-                                Button (action: {
-                                    register() { isRegistered in
-                                        self.isActiveForRegister = isRegistered
-                                    }
-                                }) {
-                                    HStack {
-                                        Spacer()
-                                        Text("Register")
-                                            .foregroundColor(Color.black)
-                                            .bold()
-                                            .padding(.horizontal, 20)
-                                            .padding(.vertical, 10)
-                                            .background(Color.white)
-                                            .cornerRadius(14)
-                                        Spacer()
-                                    }
-                                }
-                            }
-                            
-                            VStack {
-                                Text("Already have an account?")
-                                    .foregroundColor(Color.white)
-                                
-                                Button {
-                                    changeModeToLogin()
-                                } label: {
-                                        Text("Log in")
-                                            .foregroundColor(Color.white)
-                                            .bold()
-                                }
-                            }
-                            
-                            Text(registerErrorMessage)
-                                .foregroundColor(Color.red)
-                                .multilineTextAlignment(.center)
-                        }
+                        .padding()
+                        .navigationBarHidden(true)
+                        .navigationBarTitle("")
                     }
-                    .padding()
-                    .navigationBarHidden(true)
-                    .navigationBarTitle("")
                 }
+                .background(Color("lightGreen")
+                                .ignoresSafeArea())
             }
-            .background(Color("lightGreen")
-                            .ignoresSafeArea())
-            //.navigationBarBackButtonHidden(true)
+            .blur(radius: showPopUpWindow ? 3 : 0)
+        
+            PopUpWindow(title: "Error", message: errorMessage, buttonText: "Okay", show: $showPopUpWindow)
+                .alignmentGuide(.top) {
+                    $0[VerticalAlignment.center]
+                }
+                .ignoresSafeArea()
         }
     }
     
     private func login(response: @escaping (_ isLoggedIn: Bool) -> Void) {
         FirebaseManager.instance.auth.signIn(withEmail: loginModel.email, password: loginModel.password) { result, error in
             if error != nil {
-                loginErrorMessage = "Incorrect email or password"
+                showPopUpWindow = true
+                errorMessage = "Incorrect email or password"
                 response(false)
             }
             else {
@@ -199,7 +200,8 @@ struct RegisterAndLoginView: View {
     private func register(response: @escaping (_ isRegistered: Bool) -> Void){
         FirebaseManager.instance.auth.createUser(withEmail: registerModel.email, password: registerModel.password) { result, error in
             if let error = error {
-                registerErrorMessage = error.localizedDescription.description
+                showPopUpWindow = true
+                errorMessage = error.localizedDescription.description
                 response(false)
             }
             else {
