372489dd5e593916e1eb1dfba4e0dbf34120f5ce
Stefania Tsvetkova <nia_flo@abv.bg>
2022-01-21 05:16:44 -0800

    Added view friend's profile functionality

--- FitnessApp/FitnessApp/Views/ProfileView.swift
+++ FitnessApp/FitnessApp/Views/ProfileView.swift
@@ -8,149 +8,160 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @State var user: UserModel = UserModel(uid: FirebaseManager.instance.auth.currentUser?.uid ?? "")
+    @State var user: UserModel //= UserModel(uid: FirebaseManager.instance.auth.currentUser?.uid ?? "")
     
     @State private var showPopUpWindow = false
     
     @State private var isActiveForSignOut = false
+    @State private var isActiveForFriendProfile = false
     
     init(userUid: String) {
-        user = UserModel(uid: userUid)
+        _user = State(initialValue: UserModel(uid: userUid))
     }
     
     var body: some View {
-        ZStack {
-            VStack(spacing: 15) {
-                HStack {
-                    if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
-                        Spacer()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-                    
-                    Image(systemName: "person.crop.circle")
-                        .renderingMode(.original)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 80, height: 80, alignment: .center)
-                    
-                    if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
-                        Spacer()
-                        
-                        VStack {
-                            NavigationLink(destination: StartView().navigationBarBackButtonHidden(true), isActive: $isActiveForSignOut) {
-                                Button {
-                                    if signOut() {
-                                        isActiveForSignOut = true
-                                    }
-                                    else {
-                                        showPopUpWindow = true
-                                    }
-                                } label: {
-                                    Text("Sign out")
-                                        .foregroundColor(Color("darkGreen"))
-                                }
-                                .frame(maxWidth: .infinity, alignment: .trailing)
-                            }
-                        }
-                        .frame(maxHeight: 80, alignment: .top)
-                    }
-                }
-                .padding()
-
-                Text(user.name)
-                    .font(.system(size: 20, weight: .bold, design: .monospaced))
-                    .foregroundColor(Color.black)
-                
-                VStack {
+        NavigationView {
+            ZStack {
+                VStack(spacing: 15) {
                     HStack {
-                        Text("Personal info")
-                            .font(.system(size: 20, weight: .semibold))
+                        if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
+                            Spacer()
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        }
                         
-                        Spacer()
+                        Image(systemName: "person.crop.circle")
+                            .renderingMode(.original)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80, alignment: .center)
                         
                         if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
-                            Button {
-                                // TODO: edit profile..
-                            } label: {
-                                Image(systemName: "square.and.pencil")
-                                    .font(.system(size: 20))
-                            }
-                            .accentColor(Color("darkGreen"))
-                        }
-                    }
-                    .padding(.bottom, 5)
-                    
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text("name:")
-                            Text("email:")
-                            Text("age:")
-                            Text("profession:")
-                        }
-                        
-                        VStack(alignment: .leading) {
-                            Text(user.name)
-                            Text(user.email)
-                            Text(user.age)
-                            Text(user.profession)
-                        }
-                    }
-                }
-                .padding()
-                .overlay(
-                RoundedRectangle(cornerRadius: 14)
-                    .stroke(Color("darkGreen"), lineWidth: 2)
-                )
-                .padding()
-                
-                VStack {
-                    HStack {
-                        Text("Friends")
-                            .font(.system(size: 20, weight: .semibold))
-                        
-                        Spacer()
-                        
-                        if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
-                            Button {
-                                // TODO: add frined..
-                            } label: {
-                                Image(systemName: "person.badge.plus")
-                                    .font(.system(size: 20))
-                            }
-                            .accentColor(Color("darkGreen"))
-                        }
-                    }
-                    .padding(.bottom, 5)
-                    
-                    List(user.friends) { friend in
-                        HStack {
-                            Image(systemName: "person.crop.circle")
-                                .font(.system(size: 30))
+                            Spacer()
                             
-                            Text(friend.name)
-                                .font(.system(size: 20))
+                            VStack {
+                                NavigationLink(destination: StartView().navigationBarBackButtonHidden(true), isActive: $isActiveForSignOut) {
+                                    Button {
+                                        if signOut() {
+                                            isActiveForSignOut = true
+                                        }
+                                        else {
+                                            showPopUpWindow = true
+                                        }
+                                    } label: {
+                                        Text("Sign out")
+                                            .foregroundColor(Color("darkGreen"))
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .trailing)
+                                }
+                            }
+                            .frame(maxHeight: 80, alignment: .top)
+                        }
+                    }
+                    .padding()
+
+                    Text(user.name)
+                        .font(.system(size: 20, weight: .bold, design: .monospaced))
+                        .foregroundColor(Color.black)
+                    
+                    VStack {
+                        HStack {
+                            Text("Personal info")
+                                .font(.system(size: 20, weight: .semibold))
                             
                             Spacer()
+                            
+                            if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
+                                Button {
+                                    // TODO: edit profile..
+                                } label: {
+                                    Image(systemName: "square.and.pencil")
+                                        .font(.system(size: 20))
+                                }
+                                .accentColor(Color("darkGreen"))
+                            }
+                        }
+                        .padding(.bottom, 5)
+                        
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("name:")
+                                Text("email:")
+                                Text("age:")
+                                Text("profession:")
+                            }
+                            
+                            VStack(alignment: .leading) {
+                                Text(user.name)
+                                Text(user.email)
+                                Text(user.age)
+                                Text(user.profession)
+                            }
                         }
                     }
+                    .padding()
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 14)
+                        .stroke(Color("darkGreen"), lineWidth: 2)
+                    )
+                    .padding()
+                    
+                    VStack {
+                        HStack {
+                            Text("Friends")
+                                .font(.system(size: 20, weight: .semibold))
+                            
+                            Spacer()
+                            
+                            if user.uid == FirebaseManager.instance.auth.currentUser?.uid {
+                                Button {
+                                    // TODO: add frined..
+                                } label: {
+                                    Image(systemName: "person.badge.plus")
+                                        .font(.system(size: 20))
+                                }
+                                .accentColor(Color("darkGreen"))
+                            }
+                        }
+                        .padding(.bottom, 5)
+                        
+                        List(user.friends) { friend in
+                            NavigationLink(destination: ProfileView(userUid: friend.uid), isActive: $isActiveForFriendProfile) {
+                                HStack {
+                                    Button {
+                                        isActiveForFriendProfile = true
+                                    } label: {
+                                        HStack {
+                                            Image(systemName: "person.crop.circle")
+                                                .font(.system(size: 30))
+                                                
+                                            Text(friend.name)
+                                                .font(.system(size: 20))
+                                        }
+                                    }
+                                    Spacer()
+                                }
+                            }
+                        }
+                    }
+                    .padding()
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 14)
+                        .stroke(Color("darkGreen"), lineWidth: 2)
+                    )
+                    .padding()
                 }
-                .padding()
-                .overlay(
-                RoundedRectangle(cornerRadius: 14)
-                    .stroke(Color("darkGreen"), lineWidth: 2)
-                )
-                .padding()
-            }
-            .blur(radius: showPopUpWindow ? 3 : 0)
-        
-            PopUpWindow(title: "Error", message: "There was an error. Try signing out later.", buttonText: "Okay", show: $showPopUpWindow)
-                .alignmentGuide(.top) {
-                    $0[VerticalAlignment.center]
-                }
-                .ignoresSafeArea()
+                .blur(radius: showPopUpWindow ? 3 : 0)
             
+                PopUpWindow(title: "Error", message: "There was an error. Try signing out later.", buttonText: "Okay", show: $showPopUpWindow)
+                    .alignmentGuide(.top) {
+                        $0[VerticalAlignment.center]
+                    }
+                    .ignoresSafeArea()
+                
+            }
+            .navigationBarHidden(true)
         }
-        .navigationBarHidden(true)
+        .navigationBarHidden(user.uid == FirebaseManager.instance.auth.currentUser?.uid)
     }
     
     private func signOut() -> Bool {
