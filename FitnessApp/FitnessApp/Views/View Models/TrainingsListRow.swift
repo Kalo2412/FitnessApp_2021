a7f5b56275d89a9a4ca1c4109ae8bf63e7d88c5a
Kaloyan <kaloyan.va@abv.bg>
2022-02-25 13:36:06 +0200

    Added the ui of the second tab bar vie with My Trainings List View

--- FitnessApp/FitnessApp/Views/View Models/TrainingsListRow.swift
+++ FitnessApp/FitnessApp/Views/View Models/TrainingsListRow.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 
 struct TrainingsListRow: View {
+    let testTraining: Training
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color("skyGreen")
+            HStack {
+                VStack(alignment: .leading, spacing: 2) {
+                    Text(testTraining.title)
+                        .font(.headline)
+                    Text(testTraining.time.addingTimeInterval(Double.random(in: 0...5000)), style: .time)
+                    Label(extractDate(),systemImage: "calendar")
+                }
+                Spacer()
+                PopUpDescription()
+            }
+            .padding()
+        }
+    }
+    
+    func extractDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMMM YYYY"
+        
+        let date = formatter.string(from: testTraining.time)
+        
+        return date
+    }
+}
+
+struct PopUpDescription: View {
+    var body: some View {
+        Menu {
+            Button("Cancel") {
+            }
+            Text("Description:\nednjednewnoewenoned\ndjkskd")
+        } label: {
+            Image(systemName: "questionmark.circle")
+                .frame(width: 50, height: 50)
+        }
     }
 }
 
 struct TrainingsListRow_Previews: PreviewProvider {
+    static var testTraining = trainings[0].training[0]
     static var previews: some View {
-        TrainingsListRow()
+        TrainingsListRow(testTraining: testTraining)
+        //PopUpDescription()
+            .previewLayout(.fixed(width: 400, height: 80))
     }
 }
+
+
--- FitnessApp/FitnessApp/Views/second tab bar/TrainingsListView.swift
+++ FitnessApp/FitnessApp/Views/second tab bar/TrainingsListView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct TrainingsListView: View {
     var body: some View {
-        Text("Trainings list..")
+        List {
+            Section(header: Text("My Trainings").font(.largeTitle), footer: Text("No more trainings").font(.title3).fontWeight(.ultraLight)) {
+                ForEach(trainings) { training in
+                    ForEach(training.training) { currentTraining in
+                        TrainingsListRow(testTraining: currentTraining)
+                            .listRowInsets(EdgeInsets(top: 10, leading: 10, bottom: 0, trailing: 10))
+                    }
+                }
+            }
+        }
+        .navigationTitle("")
+        .navigationBarHidden(true)
     }
 }
 
