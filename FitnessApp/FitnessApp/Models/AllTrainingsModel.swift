7a0e37f8d710dfe7b69d5251ac70eecdd9b9598a
Stefania Tsvetkova <nia_flo@abv.bg>
2022-02-25 06:15:20 -0800

    Fixed problem with signed out user's trainings

--- FitnessApp/FitnessApp/Models/AllTrainingsModel.swift
+++ FitnessApp/FitnessApp/Models/AllTrainingsModel.swift
@@ -18,21 +18,23 @@ class AllTrainingsModel: ObservableObject {
     public func getAllTrainings() {
         self.trainings = []
         
-        FirebaseManager.instance.firestore.collection("users").document(FirebaseManager.instance.auth.currentUser?.uid ?? "").collection("trainings").getDocuments() { snapshot, error in
-            if error != nil {
-                return
-            }
-            
-            for document in snapshot!.documents {
-                let id = document.documentID
+        if let currentUserUid = FirebaseManager.instance.auth.currentUser?.uid {
+            FirebaseManager.instance.firestore.collection("users").document(currentUserUid).collection("trainings").getDocuments() { snapshot, error in
+                if error != nil {
+                    return
+                }
                 
-                let data = document.data()
-                
-                let title = data["title"] as? String ?? ""
-                let description = data["description"] as? String ?? ""
-                let time = (data["time"] as? Timestamp)?.dateValue() ?? Date()
-                
-                self.trainings.append(TrainingModel(id: id, title: title, description: description, time: time))
+                for document in snapshot!.documents {
+                    let id = document.documentID
+                    
+                    let data = document.data()
+                    
+                    let title = data["title"] as? String ?? ""
+                    let description = data["description"] as? String ?? ""
+                    let time = (data["time"] as? Timestamp)?.dateValue() ?? Date()
+                    
+                    self.trainings.append(TrainingModel(id: id, title: title, description: description, time: time))
+                }
             }
         }
     }
